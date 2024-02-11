dd2f9132721d8cf0bb653b69ed201e4e40863be3
Jelena Petrovic <jelena.petrovic1517@gmail.com>
2024-02-10 19:41:49 -0500

    Init project

---
+++ SolitaireGame/src/app/MainApp.java
@@ -0,0 +1,23 @@
+package app;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Swing Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
+
---
+++ SolitaireGame/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SolitaireGame {
+	requires java.desktop;
+}
